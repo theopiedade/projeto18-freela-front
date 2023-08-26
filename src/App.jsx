8c48e17cac2f666af5aa46870de20010ac264939
Theo <theo.piedade@gmail.com>
2023-08-26 13:24:39 -0300

    Dotenv OK

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
 import SignUpPage from "./pages/SignUpPage"
 import TransactionsPage from "./pages/TransactionPage"
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default function App() {
   return (
